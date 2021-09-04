f8c084b63c5cadd74341010c8a6fd038fa23c8a3
Sushrut1101 <guptasushrut@gmail.com>
2021-09-04 18:06:51 +0530

    m20lte: Properly Format vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,77 +17,104 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
-
 FDEVICE="m20lte"
 
-# m20lte specific
-export TARGET_DEVICE_ALT="m20lte"
-export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/RECOVERY"
-export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/SYSTEM"
-export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/VENDOR"
-export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/BOOT"
-export OF_AB_DEVICE=0
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=0
-export OF_FLASHLIGHT_ENABLE="0"
-export OF_SCREEN_H=2340
-export OF_STATUS_H=80
-export OF_STATUS_INDENT_LEFT=55
-export OF_STATUS_INDENT_RIGHT=55
-export OF_TWRP_COMPATIBILITY_MODE=0
-export OF_DEVICE_WITHOUT_PERSIST=1
-export OF_SKIP_FBE_DECRYPTION=1
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
 
-# R11
-export FOX_R11="1"
-export FOX_VERSION="R11.1"
-export OF_MAINTAINER="Sushrut1101"
-export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
-export FOX_ADVANCED_SECURITY="1"
-export OF_USE_TWRP_SAR_DETECT="1"
-export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
-export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
 
-export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	
+	# Base
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export LC_ALL="C"
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+	
+	# m20lte specific
+	export TARGET_DEVICE_ALT="m20lte"
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/RECOVERY"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/SYSTEM"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/VENDOR"
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/BOOT"
+	export OF_AB_DEVICE=0
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=0
+	export OF_FLASHLIGHT_ENABLE="0"
+	export OF_TWRP_COMPATIBILITY_MODE=0
+	export OF_DEVICE_WITHOUT_PERSIST=1
+	export OF_SKIP_FBE_DECRYPTION=1
+	
+	# Screen Settings
+	export OF_SCREEN_H=2340
+	export OF_STATUS_H=81
+	export OF_STATUS_INDENT_LEFT=55 # m20lte Specific
+	export OF_STATUS_INDENT_RIGHT=55 # m20lte Specific
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
+	
+	# R11.1 Settings
+	export FOX_R11="1"
+	export FOX_VERSION="R11.1"
+	export OF_MAINTAINER="Sushrut1101"
+	export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
+	export FOX_BUILD_TYPE="Stable"
+	
+	export FOX_ADVANCED_SECURITY="1"
+	export OF_USE_TWRP_SAR_DETECT="1"
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
 
-# Common
-export TARGET_ARCH=arm64
-export ALLOW_MISSING_DEPENDENCIES=true
-export LC_ALL="C"
-export TW_DEFAULT_LANGUAGE="en"
-export OF_KEEP_FORCED_ENCRYPTION="1"
-export OF_PATCH_AVB20="1"
-export OF_USE_MAGISKBOOT="1"
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-export FOX_USE_LZMA_COMPRESSION="1"
+	# Quick Backup Defaults
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	
+	# Common
+	export TARGET_ARCH=arm64
+	export TW_DEFAULT_LANGUAGE="en"
+	export OF_KEEP_FORCED_ENCRYPTION="1"
+	export OF_PATCH_AVB20="1"
+	export OF_USE_MAGISKBOOT="1"
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+	export FOX_USE_LZMA_COMPRESSION="1"
+	
+	export FOX_USE_BASH_SHELL="1"
+	export FOX_ASH_IS_BASH="1"
+	export FOX_USE_NANO_EDITOR="1"
+	export FOX_USE_TAR_BINARY="1"
+	export FOX_USE_ZIP_BINARY="1"
+	export FOX_REPLACE_BUSYBOX_PS="1"
+	export OF_USE_NEW_MAGISKBOOT="1"
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+	
+	# Use the System (ROM) Fingerprint where Available
+	export OF_USE_SYSTEM_FINGERPRINT="1"
 
-export FOX_USE_BASH_SHELL="1"
-export FOX_ASH_IS_BASH="1"
-export FOX_USE_NANO_EDITOR="1"
-export FOX_USE_TAR_BINARY="1"
-export FOX_USE_ZIP_BINARY="1"
-export FOX_REPLACE_BUSYBOX_PS="1"
-export OF_USE_NEW_MAGISKBOOT="1"
-export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
-export OF_CLOCK_POS=1
-export OF_ALLOW_DISABLE_NAVBAR=0
+	# Run Post Format Process for MTP
+	export OF_RUN_POST_FORMAT_PROCESS=1
+	
+	# Create Odin Flashable .tar files
+	export OF_NO_SAMSUNG_SPECIAL=0
 
-export FOX_BUILD_TYPE="Stable"
+	# Let's See what are our Build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
 
-# use system (ROM) fingerprint where available
-export OF_USE_SYSTEM_FINGERPRINT="1"
-
-# Run Post Format Process for MTP
-export OF_RUN_POST_FORMAT_PROCESS=1
-
-# Misc.
-export OF_CHECK_OVERWRITE_ATTEMPTS=1
-
-# Create Odin Flashable .tar files
-export OF_NO_SAMSUNG_SPECIAL=0
-
-add_lunch_combo omni_"$FDEVICE"-eng
+	add_lunch_combo omni_"$FDEVICE"-eng
+fi
+#
