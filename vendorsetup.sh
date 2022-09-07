e645eb49dceb745ad08548df6d8fd4fa21194f6d
Sushrut1101 <guptasushrut@gmail.com>
2022-09-07 17:43:39 +0530

    m20lte: Set the Variant to Dynamic
    * And Release it as Beta for now
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,10 +57,10 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_ALLOW_DISABLE_NAVBAR=0
 	
 	# R11.1 Settings
-	export FOX_R11="1"
 	export FOX_VERSION="R11.1_1"
 	export OF_MAINTAINER="Sushrut1101"
-	export FOX_BUILD_TYPE="Stable"
+	export FOX_BUILD_TYPE="Beta"
+	export FOX_VARIANT="Dynamic"
 	
 	export FOX_ADVANCED_SECURITY="1"
 	export OF_USE_TWRP_SAR_DETECT="1"
